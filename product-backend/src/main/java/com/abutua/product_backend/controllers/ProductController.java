0dfbd47363377ee2ef7f9153d80d2398364dc6a1
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-10-30 18:00:36 -0300

    Add endpoint products

--- product-backend/src/main/java/com/abutua/product_backend/controllers/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/controllers/ProductController.java
@@ -1,5 +1,8 @@
 package com.abutua.product_backend.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,22 @@ public class ProductController {
     
     @GetMapping("product")
     public Product getProduct(){
-        Product p = new Product(1, "leite", 4.50);
+        Product p = new Product(1, "product 1", 1);
 
         return p;
+    }
+
+    @GetMapping("products")
+    public List<Product> getProducts(){
+        Product p1 = new Product(1, "product 1", 1);
+        Product p2 = new Product(2, "product 2", 2);
+        Product p3 = new Product(3, "product 3", 3);
+
+        List<Product> listProd = new ArrayList<Product>();
+        listProd.add(p1);
+        listProd.add(p2);
+        listProd.add(p3);
+
+        return listProd;
     }
 }
