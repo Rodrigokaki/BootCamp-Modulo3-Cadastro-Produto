dfcfd60b1b75dc57a1e7f0139423038e91743c20
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2024-11-03 16:25:20 -0300

    adds category endpoint

--- product-backend/src/main/java/com/abutua/product_backend/models/Category.java
+++ product-backend/src/main/java/com/abutua/product_backend/models/Category.java
@@ -1,33 +1,29 @@
 package com.abutua.product_backend.models;
 
-public class Product {
+public class Category {
     private int id;
     private String name;
-    private double price;
 
-    public Product(int id, String name, double price){
+    public Category(int id, String name){
         this.id = id;
         this.name = name;
-        this.price = price;
     }
-    
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
+
     
 }
